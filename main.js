73f6b92d494b2b5c1c6b41872f04e533ff66c92e
Diego Santos <diego449diego@gmail.com>
2024-01-11 14:22:58 -0300

    adicionando as animações

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+AOS.init();
+
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]')
